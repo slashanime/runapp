44c4ed49ff55da5462160d8de7d3d7afa31cd8da
dilli <eraeren715@gmail.com>
2021-10-02 17:25:16 +0545

    initial commit

--- runapp/settings.py
+++ runapp/settings.py
@@ -123,6 +123,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+# STATIC ROOT SETTINGS
+
+STATIC_ROOT = os.path(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
